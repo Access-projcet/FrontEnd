b1b1c9f2b957beacb370337a72f288256bc56127
akfangus <akfanakfan@naver.com>
2023-03-27 03:12:05 +0900

    # FEAT : 로그인 페이지 기능 추가
    ## 로그인페이지 material-ui사용하여 작업 완료
    
    > 간단한 애니매이션 추가
    > Tab으로 Guest / Admin 로그인 페이지 분리
    > 회원가입으로 이동하는 링크 설정

---
+++ src/components/AdminLoginForm.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { TextField, Button, Link, Grid } from "@mui/material";
+import styled, { keyframes } from "styled-components";
+
+export default function AdminLoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const HandlerGuestLogin = (e) => {
+    e.preventDefault();
+    console.log("로그인");
+  };
+
+  return (
+    <DivLoginContainer>
+      <form onSubmit={HandlerGuestLogin}>
+        <TextField
+          margin="normal"
+          label="Email"
+          autoComplete="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          autoFocus
+          fullWidth
+        />
+        <TextField
+          margin="normal"
+          label="비밀번호"
+          type="password"
+          autoComplete="current-password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          fullWidth
+        />
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          fullWidth
+          sx={{ mt: 2, mb: 2 }}
+          size="large"
+        >
+          관리자 로그인
+        </Button>
+      </form>
+
+      <Grid container>
+        <Grid item xs>
+          <Link href="#" underline="hover">
+            아이디 & 비밀번호 찾기
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="/admin/signup" underline="hover">
+            관리자 회원가입
+          </Link>
+        </Grid>
+      </Grid>
+    </DivLoginContainer>
+  );
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const DivLoginContainer = styled.div`
+  width: 500px;
+  height: 500px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  animation: ${fadeIn} 1s linear;
+
+  /* gap: 10px; */
+`;
