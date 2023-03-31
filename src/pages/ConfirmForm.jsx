b4e7b26c99001fb4475020cb165cbad7a971a1bf
chanyeol123 <chanyeol123@gmail.com>
2023-04-01 07:26:16 +0900

    FEAT: 방문 신청 폼 css 수정 완료

--- src/pages/ConfirmForm.jsx
+++ src/pages/ConfirmForm.jsx
@@ -68,68 +68,81 @@ const ConfirmForm = ({ onClose }) => {
     <>
       <Header>방문 양식</Header>
       <MainWrapper>
-        <Main>
-          <label htmlFor="location">방문지역 &nbsp;&nbsp;</label>
+        <Main1>
+          <label htmlFor="location">방문지역</label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              marginRight: "75px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="location"
             value={location}
             onChange={(e) => {
               setLocation(e.target.value);
             }}
           />
-          &nbsp;&nbsp;
-          <label htmlFor="place">방문장소 &nbsp;&nbsp;</label>
+
+          <label htmlFor="place">방문장소</label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id={place}
             value={place}
             onChange={(e) => {
               setPlace(e.target.value);
             }}
           />
-          &nbsp;&nbsp;
-          <label htmlFor="target">찾아갈 분 &nbsp;&nbsp;</label>
+        </Main1>
+        <Main2>
+          <label htmlFor="target">찾아갈 분</label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              marginRight: "90px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="target"
             value={target}
             onChange={(e) => {
               setTarget(e.target.value);
             }}
           />
-          &nbsp;&nbsp;
-          <label htmlFor="purpose">목적 &nbsp;&nbsp;</label>
+
+          <label htmlFor="purpose">목적</label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="purpose"
             value={purpose}
             onChange={(e) => {
               setPurpose(e.target.value);
             }}
           />
-        </Main>
-
-        <TimeTable>
-          {/* <label htmlFor="startDate">시작날짜:</label>
-        <DatePicker
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          timeInputLabel="Time:"
-          dateFormat="yyyy-mm-dd hh-mm aa"
-          showTimeInput
-        /> */}
-          {/* <label htmlFor="endDate">종료날짜:</label>
-        <DatePicker
-          selected={endDate}
-          onChange={(date) => setEndDate(date)}
-          timeInputLabel="Time:"
-          dateFormat="yyyy-mm-dd hh-mm aa"
-          showTimeInput
-        /> */}
-          <label htmlFor="startDate">방문 날짜 &nbsp;&nbsp;</label>
+        </Main2>
+        <hr />
+        <TimeTable1>
+          <label htmlFor="startDate">방문 날짜 </label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              marginRight: "50px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="startDate"
             type="date"
             value={startDate}
@@ -137,10 +150,15 @@ const ConfirmForm = ({ onClose }) => {
               setStartDate(e.target.value);
             }}
           ></input>
-          &nbsp;&nbsp;
-          <label htmlFor="방문시간">방문 시간 &nbsp;&nbsp;</label>
+
+          <label htmlFor="방문시간">방문 시간 </label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="startTime"
             value={startTime}
             placeholder="00:00"
@@ -148,10 +166,24 @@ const ConfirmForm = ({ onClose }) => {
               setStartTime(e.target.value);
             }}
           ></input>
-          &nbsp;&nbsp;
-          <label htmlFor="endDate">종료 날짜 &nbsp;&nbsp;</label>
+        </TimeTable1>
+        <span
+          style={{
+            marginLeft: "370px",
+          }}
+        >
+          ⁓
+        </span>
+        <TimeTable2>
+          <label htmlFor="endDate">종료 날짜 </label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              marginRight: "50px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="endDate"
             type="date"
             value={endDate}
@@ -159,10 +191,15 @@ const ConfirmForm = ({ onClose }) => {
               setEndDate(e.target.value);
             }}
           ></input>
-          &nbsp;&nbsp;
-          <label htmlFor="endTime">종료 시간 &nbsp;&nbsp;</label>
+
+          <label htmlFor="endTime">종료 시간 </label>
           <input
-            style={{ width: "200px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="endTime"
             value={endTime}
             placeholder="00:00"
@@ -170,27 +207,40 @@ const ConfirmForm = ({ onClose }) => {
               setEndTime(e.target.value);
             }}
           ></input>
-          <Msg>
-            <p>
-              * 시간은 24시간 기준으로 입력해주세요. 예시 2023-03-30, 13:40,
-              2023-03-31, 14:00
-            </p>
-          </Msg>
-        </TimeTable>
+        </TimeTable2>
+        <Msg>
+          <p>
+            * 시간은 24시간 기준으로 입력해주세요. 예시 2023-03-30, 13:40,
+            2023-03-31, 14:00
+          </p>
+        </Msg>
+
+        <hr />
         <Visitor>
-          <label htmlFor="visitor">이름 &nbsp;&nbsp;</label>
+          <label htmlFor="visitor">이름 </label>
           <input
-            style={{ width: "400px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              marginRight: "110px",
+              width: "150px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="visitor"
             value={visitor}
             onChange={(e) => {
               setVisitor(e.target.value);
             }}
           />
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <label htmlFor="phoneNum">전화번호 &nbsp;&nbsp;</label>
+
+          <label htmlFor="phoneNum">전화번호</label>
           <input
-            style={{ width: "400px", height: "30px", fontSize: "20px" }}
+            style={{
+              marginLeft: "10px",
+              width: "200px",
+              height: "30px",
+              fontSize: "20px",
+            }}
             id="phoneNum"
             value={phoneNum}
             onChange={(e) => {
@@ -210,12 +260,14 @@ export default ConfirmForm;
 const Header = styled.div`
   background: grey;
   position: relative;
-  top: 0;
-  width: 100;
+  left: 0;
+  top: 25px;
+  width: 730px;
   display: flex;
   color: white;
   padding: 20px;
-  border-radius: 4px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
   font-size: larger;
   font-weight: bold;
 `;
@@ -229,25 +281,47 @@ const MainWrapper = styled.div`
   font-style: normal;
   font-weight: 700;
   font-size: 16px;
+  width: 100%;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 `;
 
-const Main = styled.div`
+const Main1 = styled.div`
+  position: relative;
+  left: -200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding-top: 50px;
+  margin: 0px -400px -50px -30px;
+`;
+
+const Main2 = styled.div`
+  position: relative;
+  left: 10px;
+  top: -200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 300px 100px -150px 30px;
+`;
+
+const TimeTable1 = styled.div`
   /* display: flex; */
   flex-direction: column;
   justify-content: center;
-  text-align: left;
   align-items: center;
-  border-bottom: 1px solid grey;
-  padding: 70px;
+  padding: 30px 0px 30px 45px;
 `;
 
-const TimeTable = styled.div`
+const TimeTable2 = styled.div`
   /* display: flex; */
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-bottom: 1px solid grey;
-  padding: 50px;
+  padding: 30px 0px 30px 45px;
 `;
 
 const Visitor = styled.div`
@@ -255,7 +329,7 @@ const Visitor = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 40px;
+  padding: 50px 0px 5px 80px;
 `;
 
 const SubmitBtn = styled.div`
@@ -272,8 +346,8 @@ const SubmitBtn = styled.div`
   padding: 15px;
 
   position: relative;
-  left: 670px;
-  top: 0;
+  left: 400px;
+  top: -25px;
   height: 25px;
   width: 100px;
 `;
@@ -292,8 +366,8 @@ const CancelBtn = styled.div`
   padding: 15px;
 
   position: relative;
-  left: 530px;
-  top: 55px;
+  left: 250px;
+  top: 30px;
   height: 25px;
   width: 100px;
 `;
